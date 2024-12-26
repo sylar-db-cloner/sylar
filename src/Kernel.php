659e041919578ac9d2c06ff34589ee1c09270498
Benoît Marcelin <benoit.marcelin@maison.fr>
2024-12-26 17:12:07 +0100

    chore: make Kernel.php look much more like MicroKernelTrait

--- src/Kernel.php
+++ src/Kernel.php
@@ -6,40 +6,15 @@ namespace App;
 
 use App\Infrastructure\CompilerPass\ConsoleCommandFilterCompilerPass;
 use App\Infrastructure\CompilerPass\GraphQLResolverCompilerPass;
-use App\Infrastructure\PostContainerDumpActions\PostContainerDumpServiceInterface;
 use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
-use Symfony\Component\Config\ConfigCache;
 use Symfony\Component\Config\Loader\LoaderInterface;
-use Symfony\Component\Config\Resource\FileResource;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 use Symfony\Component\HttpKernel\Kernel as BaseKernel;
 
 class Kernel extends BaseKernel
 {
     use MicroKernelTrait;
-
-    private const CONFIG_EXTS = '.{php,xml,yaml,yml}';
-
-    public function getProjectDir(): string
-    {
-        return __DIR__ . '/../';
-    }
-
-    public function registerBundles(): iterable
-    {
-        $contents = require $this->getProjectDir() . '/config/bundles.php';
-        foreach ($contents as $class => $envs) {
-            if ($envs[$this->environment] ?? $envs['all'] ?? false) {
-                yield new $class();
-            }
-        }
-    }
-
-    protected function dumpContainer(ConfigCache $cache, ContainerBuilder $container, string $class, string $baseClass): void
-    {
-        parent::dumpContainer($cache, $container, $class, $baseClass);
-        $this->postDumpContainerAction($container);
-    }
 
     public function build(ContainerBuilder $container): void
     {
@@ -49,23 +24,21 @@ class Kernel extends BaseKernel
         $container->addCompilerPass(new GraphQLResolverCompilerPass());
     }
 
-    protected function configureContainer(ContainerBuilder $container, LoaderInterface $loader): void
+    protected function configureContainer(ContainerConfigurator $container, LoaderInterface $loader): void
     {
-        $container->addResource(new FileResource($this->getProjectDir() . '/config/bundles.php'));
-        $container->setParameter('container.dumper.inline_class_loader', \PHP_VERSION_ID < 70400 || $this->debug);
-        $container->setParameter('container.dumper.inline_factories', true);
-        $confDir = realpath($this->getProjectDir() . '/config');
+        $configDir = $this->getConfigDir();
 
-        $loader->load($confDir . '/{packages}/*' . self::CONFIG_EXTS, 'glob');
-        $loader->load($confDir . '/{packages}/' . $this->environment . '/*' . self::CONFIG_EXTS, 'glob');
-        $loader->load($confDir . '/services/*' . self::CONFIG_EXTS, 'glob');
-        $loader->load($confDir . '/services/' . $this->environment . '/*' . self::CONFIG_EXTS, 'glob');
-    }
+        $container->import($configDir . '/{packages}/*.{php,yaml}');
+        $container->import($configDir . '/{packages}/' . $this->environment . '/*.{php,yaml}');
+        $container->import($configDir . '/services/*.{php,yaml}');
+        $container->import($configDir . '/services/' . $this->environment . '/*.{php,yaml}');
 
-    private function postDumpContainerAction(ContainerBuilder $container): void
-    {
-        /** @var PostContainerDumpServiceInterface $postContainerDumpAction */
-        $postContainerDumpAction = $container->get(PostContainerDumpServiceInterface::class);
-        $postContainerDumpAction->execute();
+        if (is_file($configDir . '/services.yaml')) {
+            $container->import($configDir . '/services.yaml');
+            $container->import($configDir . '/{services}_' . $this->environment . '.yaml');
+        } else {
+            $container->import($configDir . '/{services}.php');
+            $container->import($configDir . '/{services}_' . $this->environment . '.php');
+        }
     }
 }
