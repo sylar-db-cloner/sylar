b522f0960f98e8459c81b7ef7173f6bf8653389c
Benoît Marcelin <benoit.marcelin@maison.fr>
2024-12-30 17:11:10 +0100

    chore: refresh graphql-codegen

--- assets/gql/gql.ts
+++ assets/gql/gql.ts
@@ -11,6 +11,7 @@ import { TypedDocumentNode as DocumentNode } from '@graphql-typed-document-node/
  * 3. It does not support dead code elimination, so it will add unused operations.
  *
  * Therefore it is highly recommended to use the babel or swc plugin for production.
+ * Learn more about it here: https://the-guild.dev/graphql/codegen/plugins/presets/preset-client#reducing-bundle-size
  */
 const documents = {
   '\n        mutation MutationForceDestroyContainer($name: String!) {\n          forceDestroyContainer(input: { name: $name }) {\n            ... on SuccessOutput {\n              success\n            }\n            ... on FailedOutput {\n              code\n            }\n          }\n        }\n      ':
